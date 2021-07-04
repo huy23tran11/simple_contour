f46fd31f4ca097798f2a53afbb5d85b272a8089f
huy23tran11 <huy23tran11@gmail.com>
2021-07-04 15:05:56 -0700

    build

---
+++ build/CMakeFiles/region_separation.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/region_separation.dir/region_separation.cpp.o"
+  "region_separation.pdb"
+  "region_separation"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/region_separation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
